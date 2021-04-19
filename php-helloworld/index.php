3b43d175011b5839ee29d66d0976276ea52e2004
Daniel <daniel@crc1.localdomain>
2021-04-19 11:50:09 +1000

    new msg

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "<H1> Hello, World! php version is </H1>" . PHP_VERSION . "\n";
-print "aint no fkn change happenin! \n";
+print "Woah mang the change! it happened \n";
+print "Doge to the moon!\n";
 ?>
