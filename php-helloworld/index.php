63191a48b2bc9dc80c295b8fcbcd74e1b97edf49
Daniel <daniel@crc1.localdomain>
2021-04-19 11:06:29 +1000

    stuff

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "<H1> Hello, World! php version is " . PHP_VERSION . </H1>"\n";
 print "aint no fkn change happenin! \n";
 ?>
